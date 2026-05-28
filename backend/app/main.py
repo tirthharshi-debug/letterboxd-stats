54032d72ce54c69387800aa241a78b7900aa9ec5
Tirth Harshi <tirth.harshi@gmail.com>
2026-05-28 16:17:27 +0530

    Update CORS for new domain

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,7 +38,7 @@ app.add_middleware(
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
-    "https://letterboxd-stats-gamma.vercel.app",
+    "https://cinestats.vercel.app/",
 ],
     allow_credentials=True,
     allow_methods=["*"],
